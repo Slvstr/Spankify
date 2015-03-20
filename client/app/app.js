f2ad1097daf41de72384b001936b52d989295df3
Erik Hellenbrand <ehellenbrand@gmail.com>
2015-03-19 19:15:41 -0500

    Add side navigation

--- client/app/app.js
+++ client/app/app.js
@@ -1,2 +1,14 @@
 
 angular.module('spankify', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ui.router', 'ngMaterial']);
+
+/******************************************************************************
+ * App-Level Controller
+ *****************************************************************************/
+angular.module('spankify')
+  .controller('appCtrl', function($mdSidenav) {
+    this.toggleNav = function() {
+      $mdSidenav('left').toggle().then(function() {
+        console.log('toggled nav');
+      });
+    };
+  });
---
+++ client/app/components/sidenav/nav.controller.js
@@ -0,0 +1,4 @@
+angular.module('spankify')
+  .controller('navCtrl', function() {
+
+  });
